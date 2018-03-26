d5bec3be381da9e0112c61216eb6a76af92e3d02
VitaliZinkevich <vitalizinkevich@gmail.com>
2018-03-26 15:14:41 +0300

    migrated to http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,8 @@ import { of } from 'rxjs/observable/of';
 
 import { MessageService } from './message.service';
 
+import { catchError, map, tap } from 'rxjs/operators';
+
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -35,16 +37,45 @@ getHeroes(): Observable<Hero[]> {
   return of(HEROES);
 }
 */
+/*
 getHero(id: number): Observable<Hero> {
   // Todo: send the message _after_ fetching the hero
   this.messageService.add(`HeroService: fetched hero id=${id}`);
   return of(HEROES.find(hero => hero.id === id));
+}
+*/
+
+/** GET hero by id. Will 404 if id not found */
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
 }
 
 /** GET heroes from the server */
 
 getHeroes (): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log(`fetched heroes`)),
+      catchError(this.handleError('getHeroes', []))
+    );
+}
+
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 }
 
 /** Log a HeroService message with the MessageService */
